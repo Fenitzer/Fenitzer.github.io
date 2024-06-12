2e6213b9ec8fef85fb641a4f57866197b72569de
Fenitzer <felipe_lomnitzer@hotmail.com>
2024-06-11 21:04:16 -0300

    Java.scrip

--- test.js
+++ test.js
@@ -4,26 +4,36 @@ function digaVar(parametro, outro)
     } 
 
 
-let pessoa1 = {
-            nome : "wagner", 
-            idade : 34, 
-            gay : true,
-            cidade : "Llanfallpushguingangogeravishtrobollantyciliogogogoh",
-            cumprimentar : function(parametro)
-            {
-                console.log(parametro+" blz?");
-            }
-             };
+let pessoa1 =   {
+                    nome : "wagner", 
+                    idade : 34, 
+                    gay : true,
+                    cidade : "Llanfallpushguingangogeravishtrobollantyciliogogogoh",
+                    cumprimentar : function(parametro)
+                    {
+                        console.log(parametro+" blz?");
+                    }
+                };
 
-let pessoa2 = {
-            nome : "felipe", 
-            idade : 35, 
-            gay : false,
-            cidade : "São Paulo",
-            cumprimentar : function(parametro)
-            {
-                console.log(parametro+" blz?");
-            }
-    };
-console.log(getElementById);
+let pessoa2 =   {
+                    nome : "felipe", 
+                    idade : 35, 
+                    gay : false,
+                    cidade : "São Paulo",
+                    cumprimentar : function(parametro)
+                    {
+                        console.log(parametro+" blz?");
+                    }
+                };
 
+let carros =    [
+                    "BMW", "Mercedes Benz", "Skoda", "Mazda", "Audi"
+                ];
+                carros.splice(3,1);
+                carros.splice(2, 0,"Volvo", "Porsche", "VW");
+
+for ( let i = 10; i >= 0; i --)
+{
+    console.log (i)
+}
+    console.log("BOOOOOM!!");
